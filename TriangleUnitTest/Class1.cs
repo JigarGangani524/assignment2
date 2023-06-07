7c15fb7d1064320df38de701c158764319e2f178
Jigar Gangani <jgangani6887@conestogac.on.ca>
2023-06-06 23:29:48 -0400

    Valid equilateral triangle

---
+++ TriangleUnitTest/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using NUnit.Framework;
+using TriangleSolver;
+
+public class TriangleTests
+{
+    /* Valid equilateral triangle */
+    [Test]
+    public void AnalyzeTriangle_ValidEquilateral_ReturnsEquilateral()
+    {
+        // Arrange
+        int firstSide = 4;
+        int secondSide = 4;
+        int thirdSide = 4;
+
+        // Act
+        string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        // Assert
+        Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
+    }
+
+
+}
+
+
+
