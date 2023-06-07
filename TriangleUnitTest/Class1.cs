a855fc240937cdba7e484733effb2df817919a50
Jigar Gangani <jgangani6887@conestogac.on.ca>
2023-06-06 23:38:40 -0400

    Valid isosceles triangle

--- TriangleUnitTest/Class1.cs
+++ TriangleUnitTest/Class1.cs
@@ -27,6 +27,53 @@ public class TriangleTests
     }
 
 
+    /* Valid isosceles triangle */
+    [Test]
+    public void AnalyzeTriangle_ValidIsosceles_ReturnsIsosceles()
+    {
+        // Arrange
+        int firstSide = 5;
+        int secondSide = 5;
+        int thirdSide = 8;
+
+        // Act
+        string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        // Assert
+        Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+    }
+
+    [Test]
+    public void AnalyzeTriangle_ValidIsosceles_ReturnsIsosceles2()
+    {
+        // Arrange
+        int firstSide = 3;
+        int secondSide = 4;
+        int thirdSide = 4;
+
+        // Act
+        string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        // Assert
+        Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+    }
+
+    [Test]
+    public void AnalyzeTriangle_ValidIsosceles_ReturnsIsosceles3()
+    {
+        // Arrange
+        int firstSide = 5;
+        int secondSide = 6;
+        int thirdSide = 5;
+
+        // Act
+        string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        // Assert
+        Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+    }
+
+
 }
 
 
